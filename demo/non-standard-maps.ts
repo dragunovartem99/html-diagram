2b563b0ad2baddf4aee85f8da1b8c9f549901dc9
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-08 13:54:02 +0300

    Add Smart

--- demo/non-standard-maps.ts
+++ demo/non-standard-maps.ts
@@ -36,7 +36,27 @@ const berlin = new Map([
 	[" ", { light: " ", dark: "+" }],
 ]);
 
+const smart = new Map([
+	// White
+	["K", { light: "F", dark: "L" }],
+	["Q", { light: "E", dark: "K" }],
+	["R", { light: "D", dark: "J" }],
+	["B", { light: "C", dark: "I" }],
+	["N", { light: "B", dark: "H" }],
+	["P", { light: "A", dark: "G" }],
+	// Black
+	["k", { light: "f", dark: "l" }],
+	["q", { light: "e", dark: "k" }],
+	["r", { light: "d", dark: "j" }],
+	["b", { light: "c", dark: "i" }],
+	["n", { light: "b", dark: "h" }],
+	["p", { light: "a", dark: "g" }],
+	// Empty
+	[" ", { light: "M", dark: "m" }],
+]);
+
 export default new Map([
 	["Alpha2", alpha2],
 	["Berlin", berlin],
+	["Smart", smart],
 ]);
