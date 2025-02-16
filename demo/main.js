e7490e2c4a66101ac7ef9568c2951d07a147e710
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-17 02:03:11 +0300

    Cleanup

--- demo/main.js
+++ demo/main.js
@@ -15,31 +15,29 @@ import { watchTV } from "./scripts/watchTV";
 	// Demo: Typography select
 	const typography = document.querySelector("#typography");
 	useTypography(typography.value);
-	typography.addEventListener("change", (event) => useTypography(event.target.value));
+	typography.addEventListener("change", ({ target }) => useTypography(target.value));
 }
 
 {
 	// Demo: Lichess stream
-	function setGameInfo({ gameId, players }) {
+	function setGameInfo({ id, players }) {
 		const [white, black] = live.querySelectorAll(".player");
 		const boardLink = live.querySelector("a");
 
-		boardLink.href = `https://lichess.org/${gameId}`;
+		boardLink.href = `https://lichess.org/${id}`;
 
 		white.textContent = players[0].user.name;
 		black.textContent = players[1].user.name;
 	}
 
 	watchTV(({ t: type, d: data }) => {
-		const { id: gameId, players, fen } = data;
-
 		const live = document.querySelector("#live");
 		const diagram = live.querySelector("my-diagram");
 
-		diagram.fen = fen;
+		diagram.fen = data.fen;
 
 		if (type === "featured") {
-			setGameInfo({ gameId, players });
+			setGameInfo(data);
 		}
 	});
 }
