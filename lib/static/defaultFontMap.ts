8d800b504812d29f525013c2e85f739b712532bc
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-20 04:31:32 +0300

    Create perfect font map

--- lib/static/defaultFontMap.ts
+++ lib/static/defaultFontMap.ts
@@ -1,20 +1,21 @@
 import type { FontMap } from "../types";
 
 export const defaultFontMap: FontMap = new Map([
-	// White
-	["K", { light: "k", dark: "K" }],
-	["Q", { light: "q", dark: "Q" }],
-	["R", { light: "r", dark: "R" }],
-	["B", { light: "b", dark: "B" }],
-	["N", { light: "n", dark: "N" }],
-	["P", { light: "p", dark: "P" }],
-	// Black
-	["k", { light: "l", dark: "L" }],
-	["q", { light: "w", dark: "W" }],
-	["r", { light: "t", dark: "T" }],
-	["b", { light: "v", dark: "V" }],
-	["n", { light: "m", dark: "M" }],
-	["p", { light: "o", dark: "O" }],
+	// White (English)
+	["K", { light: "K", dark: "k" }],
+	["Q", { light: "Q", dark: "q" }],
+	["R", { light: "R", dark: "r" }],
+	["B", { light: "B", dark: "b" }],
+	["N", { light: "N", dark: "n" }],
+	["P", { light: "P", dark: "p" }],
+	// Black (Russian)
+	["k", { light: "Ц", dark: "ц" }], // Царь
+	["q", { light: "Ф", dark: "ф" }],
+	["r", { light: "Л", dark: "л" }],
+	["b", { light: "С", dark: "с" }],
+	["n", { light: "К", dark: "к" }],
+	["p", { light: "П", dark: "п" }],
 	// Empty
-	[" ", { light: "*", dark: "+" }],
+	[" ", { light: "+", dark: "-" }],
 ]);
+
