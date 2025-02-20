062ebf568141f959a6833b60dd710647bfe56067
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-20 06:08:40 +0300

    Include original font and readme

--- demo/scripts/setTypography.ts
+++ demo/scripts/setTypography.ts
@@ -1,26 +1,29 @@
 import type { FontName } from "../../lib/types";
 
-function setAppStyle(font: FontName) {
-	const app = document.querySelector("#app")!;
-
-	if (font === "Assistant") {
-		app.setAttribute("style", `--diagram-font: Diagram ${font}; --diagram-spacing: -0.4cqw`);
-	} else {
-		app.setAttribute("style", `--diagram-font: Diagram ${font}`);
-	}
-}
-
 function registerFontFamily(font: FontName) {
 	const stylesheet = new CSSStyleSheet();
 
-	const fontFamily = `Diagram ${font}`;
-	const fontFile = `diagram-${font.toLowerCase().replace(" ", "-")}.woff2`;
+	const fullName = `Diagram${font}`;
+	const dirName = `./woff2/${fullName}`;
+	const fileName = `${fullName}.woff2`;
 
 	stylesheet.insertRule(
-		`@font-face { font-family: "${fontFamily}"; src: url("./woff2/${fontFile}"); font-display: block; }`
+		`@font-face { font-family: "Diagram ${font}"; src: url("${dirName}/${fileName}"); font-display: block; }`
 	);
 
 	document.adoptedStyleSheets.push(stylesheet);
+}
+
+function setAppStyle(font: FontName) {
+	const app = document.querySelector("#app")!;
+
+	let style = `--diagram-font: Diagram ${font};`;
+
+	if (font === "Assistant") {
+		style += " --diagram-spacing: -0.4cqw";
+	}
+
+	app.setAttribute("style", style);
 }
 
 export function setTypography(font: FontName) {
