8d659996a4ad84e873693d5a0fdbb0cbf108ee94
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-01 10:05:41 +0300

    Change base for 100th time

--- demo.config.ts
+++ demo.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 
-let base = "/html-diagram/";
-
-if (process.env.PREVIEW_PATH) {
-	base += `${process.env.PREVIEW_PATH}`;
-}
-
 export default defineConfig({
-	base,
+	base: "./",
 	build: { outDir: "dist-demo" },
 });
