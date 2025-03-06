b92e6f502f8bd268217927dc75d33e727639b821
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-06 09:39:28 +0300

    Write down all CSS

--- lib/static/getBoardCSS.ts
+++ lib/static/getBoardCSS.ts
@@ -6,41 +6,70 @@ const css = `
 		grid-auto-rows: 1fr;
 	}
 
-	:host > * {
+	div {
 		user-select: none;
 		line-height: 1;
 		color: var(--diagram-stroke, black);
 		font-size: var(--diagram-webkit-unit, 12.5cqw);
-		margin: -0.1cqw;
 		font-family: var(--diagram-font);
-		position: relative;
 	}
-	           *::before { content: "+" }
-	      [dark]::before { content: "=" }
-	      [is=P]::before { content: "P" }
-	[dark][is=P]::before { content: "p" }
-	      [is=N]::before { content: "N" }
-	[dark][is=N]::before { content: "n" }
-	      [is=B]::before { content: "B" }
-	[dark][is=B]::before { content: "b" }
-	      [is=R]::before { content: "R" }
-	[dark][is=R]::before { content: "r" }
-	      [is=Q]::before { content: "Q" }
-	[dark][is=Q]::before { content: "q" }
-	      [is=K]::before { content: "K" }
-	[dark][is=K]::before { content: "k" }
-	      [is=k]::before { content: "Ц" }
-	[dark][is=k]::before { content: "ц" }
-	      [is=q]::before { content: "Ф" }
-	[dark][is=q]::before { content: "ф" }
-	      [is=r]::before { content: "Л" }
-	[dark][is=r]::before { content: "л" }
-	      [is=b]::before { content: "С" }
-	[dark][is=b]::before { content: "с" }
-	      [is=n]::before { content: "К" }
-	[dark][is=n]::before { content: "к" }
-	      [is=p]::before { content: "П" }
-	[dark][is=p]::before { content: "п" }
+
+	div::before { content: "+" } [dark]::before { content: "=" }
+
+	[is=K]::before { content: "K" } [is=Q]::before { content: "Q" }
+	[is=R]::before { content: "R" } [is=B]::before { content: "B" }
+	[is=N]::before { content: "N" } [is=P]::before { content: "P" }
+	[is=k]::before { content: "Ц" } [is=q]::before { content: "Ф" }
+	[is=r]::before { content: "Л" } [is=b]::before { content: "С" }
+	[is=n]::before { content: "К" } [is=p]::before { content: "П" }
+
+	[dark][is=K]::before { content: "k" } [dark][is=Q]::before { content: "q" }
+	[dark][is=R]::before { content: "r" } [dark][is=B]::before { content: "b" }
+	[dark][is=N]::before { content: "n" } [dark][is=P]::before { content: "p" }
+	[dark][is=k]::before { content: "ц" } [dark][is=q]::before { content: "ф" }
+	[dark][is=r]::before { content: "л" } [dark][is=b]::before { content: "с" }
+	[dark][is=n]::before { content: "к" } [dark][is=p]::before { content: "п" }
+	
+	:host([colored]) div { position: relative }
+
+	:host([colored]) div::after {
+		content: "";
+		position: absolute;
+		inset: 0;
+		z-index: -1;
+		background-color: var(--diagram-light, white);
+		color: var(--diagram-fill, white);
+	}
+
+	:host([colored]) [dark]::after { background: var(--diagram-dark, lightgray) }
+	:host([colored]) [dark]::before { content: "+" }
+
+	:host([colored]) [dark][is=K]::before { content: "K" }
+	:host([colored]) [dark][is=Q]::before { content: "Q" }
+	:host([colored]) [dark][is=R]::before { content: "R" }
+	:host([colored]) [dark][is=B]::before { content: "B" }
+	:host([colored]) [dark][is=N]::before { content: "N" }
+	:host([colored]) [dark][is=P]::before { content: "P" }
+
+	:host([colored]) [dark][is=k]::before { content: "Ц" }
+	:host([colored]) [dark][is=q]::before { content: "Ф" }
+	:host([colored]) [dark][is=r]::before { content: "Л" }
+	:host([colored]) [dark][is=b]::before { content: "С" }
+	:host([colored]) [dark][is=n]::before { content: "К" }
+	:host([colored]) [dark][is=p]::before { content: "П" }
+
+	:host([colored]) [is=K]::after { content: "0" }
+	:host([colored]) [is=Q]::after { content: "1" }
+	:host([colored]) [is=R]::after { content: "2" }
+	:host([colored]) [is=B]::after { content: "3" }
+	:host([colored]) [is=N]::after { content: "4" }
+	:host([colored]) [is=P]::after { content: "5" }
+	:host([colored]) [is=k]::after { content: "6" }
+	:host([colored]) [is=q]::after { content: "7" }
+	:host([colored]) [is=r]::after { content: "8" }
+	:host([colored]) [is=b]::after { content: "9" }
+	:host([colored]) [is=n]::after { content: ":" }
+	:host([colored]) [is=p]::after { content: ";" }
 `;
 
 export function getBoardCSS() {
