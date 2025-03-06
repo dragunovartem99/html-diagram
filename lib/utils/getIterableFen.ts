fdfcca5ac3c98c31feb72da797468346af27b1bf
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-06 11:31:16 +0300

    Change comments explaining fen work

--- lib/utils/getIterableFen.ts
+++ lib/utils/getIterableFen.ts
@@ -11,8 +11,8 @@ export function getIterableFen({ fen, reversed }: Options) {
 
 function expandPiecePlecement(fen: FenRecord) {
 	return fen
-		.split(" ")[0] // extract position
-		.replace(/\//g, "")
-		.replace(/\d/g, (digit) => "1".repeat(+digit)) // expand numbers, e.g. "3" => "111"
-		.slice(0, 64); // cut possible extra symbols (e.g. Crazyhouse)
+		.split(" ")[0] // get piece placement
+		.replace(/\//g, "") // remove slashes
+		.replace(/\d/g, (digit) => "1".repeat(+digit)) // expand numbers, e.g. "5" => "11111"
+		.slice(0, 64); // cut possible extra symbols (like Crazyhouse)
 }
