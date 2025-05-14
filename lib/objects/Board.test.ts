aec2b692c7060b90bfa2cdb7adbbec51df4ae1ba
Artem Dragunov <dragunovartem99@gmail.com>
2025-05-14 12:18:24 +0300

    Add basic tests for board class

---
+++ lib/objects/Board.test.ts
@@ -0,0 +1,47 @@
+// @vitest-environment jsdom
+
+import { expect, test } from "vitest";
+import { Board } from "./Board";
+import { IBoard } from "../types";
+
+const extractIterableFen = (board: IBoard) =>
+	[...board.html].map((el) => el.getAttribute("is") ?? " ");
+
+test("Generates squares internally", () => {
+	const board = new Board();
+	expect(board.html.length).toBe(64);
+});
+
+test("Sets the fen", () => {
+	const fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+	const board = new Board();
+
+	board.fen = fen;
+
+	// prettier-ignore
+	const sicilianDefence = [
+		"r", "n", "b", "q", "k", "b", "n", "r",
+		"p", "p", " ", "p", "p", "p", "p", "p",
+		" ", " ", " ", " ", " ", " ", " ", " ",
+		" ", " ", "p", " ", " ", " ", " ", " ",
+		" ", " ", " ", " ", "P", " ", " ", " ",
+		" ", " ", " ", " ", " ", " ", " ", " ",
+		"P", "P", "P", "P", " ", "P", "P", "P",
+		"R", "N", "B", "Q", "K", "B", "N", "R",
+	];
+
+	expect(extractIterableFen(board).join() === sicilianDefence.join()).toBe(true);
+});
+
+test("Flips the board", () => {
+	const fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+	const board = new Board();
+
+	board.fen = fen;
+	const iterableFen = extractIterableFen(board);
+
+	board.flipped = true;
+	const reversedIterableFen = extractIterableFen(board);
+
+	expect(iterableFen.reverse().join() === reversedIterableFen.join()).toBe(true);
+});
